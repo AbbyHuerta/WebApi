a96d2d020b23a227e8b8567838f16b7a004289d5
Abigail7SJRVJJJ <104639718+Abigail7SJRVJJJ@users.noreply.github.com>
2025-03-22 21:30:13 -0600

    Agregar archivos de proyecto.

---
+++ PracticaWebApiSonia/Model/ProductCategory.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticaWebApiSonia.Model
+{
+    public class ProductCategory
+    {
+        [Table("ProductCategory", Schema = "Production")]
+        
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int ProductCategoryID { get; set; }
+
+            [Required]
+            [Column(TypeName = "dbo.Name")]
+            public string Name { get; set; }
+
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            public Guid RowGuid { get; set; } = Guid.NewGuid();
+
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    }
+}
+
